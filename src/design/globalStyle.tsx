976707166bcfcfc1f994b863bf2530b9fc32cd38
pniebrzydowski <patrick.niebrzydowski@gmail.com>
2020-05-15 12:02:16 +0200

    move index css to global style

--- src/design/globalStyle.tsx
+++ src/design/globalStyle.tsx
@@ -16,6 +16,21 @@ const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
     background-color: ${(props) => props.theme.colors.xLight};
     color: ${(props) => props.theme.colors.xDark};
     min-height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
   }
 
   h1, h2, h3, h4, h5, h6, p, ul, li {
@@ -56,7 +71,7 @@ const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
     color: ${(props) => props.theme.colors.secondary}
   }
 
-  ul {
+  ul, li {
     list-style: none;
   }
 `;
