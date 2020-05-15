d4dc9b76ae42a5691b5f512600daca6b49a853f2
pniebrzydowski <patrick.niebrzydowski@gmail.com>
2020-05-15 12:04:39 +0200

    add router and padding to global storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 
 import { withKnobs } from '@storybook/addon-knobs';
 import { addDecorator } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '../src/design/globalStyle';
 import theme from '../src/design/theme';
 
 addDecorator((storyFn: () => React.ReactNode) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    {storyFn()}
-  </ThemeProvider>
+  <div style={{ padding: 10 }}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {storyFn()}
+      </ThemeProvider>
+    </BrowserRouter>
+  </div>
 ));
 
 addDecorator(withKnobs);
