1cce89441aa5363d95d1c17196494b8b716b7a64
pniebrzydowski <patrick.niebrzydowski@gmail.com>
2020-06-19 16:36:35 +0200

    fix new series test

--- src/components/series/NewSeries/NewSeries.test.tsx
+++ src/components/series/NewSeries/NewSeries.test.tsx
@@ -1,65 +1,106 @@
 import React from 'react';
 
+import { FirebaseContext } from '../../../firebase';
+import * as useAdminCheck from '../../../firebase/hooks/useAdminCheck';
 import { getFutureDate, getToday } from '../../../utils/date';
-import { render, screen, userEvent, wait } from '../../../utils/tests';
+import {
+  BaseFirebaseMock,
+  render,
+  screen,
+  userEvent,
+  wait
+} from '../../../utils/tests';
 import NewSeries from './NewSeries';
 
-jest.mock("../../../firebase/hooks/useAdminCheck", () => jest.fn(() => true));
+class FirebaseMock extends BaseFirebaseMock {
+  constructor() {
+    super();
+    this.firestore.collection = () => {
+      return {
+        add: jest.fn(),
+      };
+    };
+  }
+}
 
-test("opens and closes the new series form", async () => {
-  render(<NewSeries />);
-  const newButton = screen.getByRole("button", { name: "Add new series" });
-  expect(newButton).toBeInTheDocument();
-  await userEvent.click(newButton);
+describe("New Series", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.spyOn(useAdminCheck, "default").mockReturnValue(true);
+  });
 
-  const saveButton = screen.getByRole("button", { name: "Save" });
-  expect(saveButton).toBeInTheDocument();
-  const cancelButton = screen.getByRole("button", { name: "Cancel" });
-  expect(cancelButton).toBeInTheDocument();
+  const firebase = new FirebaseMock();
 
-  await userEvent.click(cancelButton);
-  expect(
-    screen.getByRole("button", { name: "Add new series" })
-  ).toBeInTheDocument();
-  expect(cancelButton).not.toBeInTheDocument();
-  expect(saveButton).not.toBeInTheDocument();
-});
+  const renderWithFirebase = () => {
+    render(
+      <FirebaseContext.Provider value={firebase}>
+        <NewSeries />
+      </FirebaseContext.Provider>
+    );
+  };
 
-test("opens and submits the form with error", async () => {
-  jest.spyOn(console, "log").mockImplementation();
+  it("opens and closes the new series form", async () => {
+    renderWithFirebase();
+    const newButton = screen.getByRole("button", { name: "Add new series" });
+    expect(newButton).toBeInTheDocument();
+    await userEvent.click(newButton);
 
-  render(<NewSeries />);
-  const newButton = screen.getByRole("button", { name: "Add new series" });
-  await userEvent.click(newButton);
+    const saveButton = screen.getByRole("button", { name: "Save" });
+    expect(saveButton).toBeInTheDocument();
+    const cancelButton = screen.getByRole("button", { name: "Cancel" });
+    expect(cancelButton).toBeInTheDocument();
 
-  const saveButton = screen.getByRole("button", { name: "Save" });
-  await userEvent.click(saveButton);
+    await userEvent.click(cancelButton);
+    expect(
+      screen.getByRole("button", { name: "Add new series" })
+    ).toBeInTheDocument();
+    expect(cancelButton).not.toBeInTheDocument();
+    expect(saveButton).not.toBeInTheDocument();
+  });
 
-  await wait();
+  it("opens and submits the form with error", async () => {
+    jest.spyOn(firebase.firestore, "collection");
 
-  expect(console.log).not.toHaveBeenCalled();
-  expect(screen.getByText("Please enter a name")).toBeInTheDocument();
-});
+    renderWithFirebase();
+    const newButton = screen.getByRole("button", { name: "Add new series" });
+    await userEvent.click(newButton);
 
-test("opens and submits the form successfully", async () => {
-  jest.spyOn(console, "log").mockImplementation();
+    const saveButton = screen.getByRole("button", { name: "Save" });
+    await userEvent.click(saveButton);
 
-  render(<NewSeries />);
-  const newButton = screen.getByRole("button", { name: "Add new series" });
-  await userEvent.click(newButton);
+    await wait();
 
-  const nameField = screen.getByLabelText("Series Name");
-  await userEvent.type(nameField, "New Series");
+    expect(firebase.firestore.collection).not.toHaveBeenCalled();
+    expect(screen.getByText("Please enter a name")).toBeInTheDocument();
+  });
 
-  const saveButton = screen.getByRole("button", { name: "Save" });
-  await userEvent.click(saveButton);
+  it("opens and submits the form successfully", async () => {
+    const set = () => new Promise((resolve) => resolve());
+    const doc = { set };
+    const collection = { doc: () => doc };
+    jest
+      .spyOn(firebase.firestore, "collection")
+      .mockImplementation(() => collection);
+    jest.spyOn(collection, "doc").mockImplementation(() => doc);
+    jest.spyOn(doc, "set");
 
-  await wait();
+    renderWithFirebase();
+    const newButton = screen.getByRole("button", { name: "Add new series" });
+    await userEvent.click(newButton);
 
-  expect(console.log).toHaveBeenCalledWith({
-    name: "New Series",
-    startDate: getToday(),
-    endDate: getFutureDate(3, "month"),
-    description: "",
+    const nameField = screen.getByLabelText("Series Name");
+    await userEvent.type(nameField, "New Series");
+
+    const saveButton = screen.getByRole("button", { name: "Save" });
+    await userEvent.click(saveButton);
+
+    await wait();
+
+    expect(doc.set).toHaveBeenCalledWith({
+      name: "New Series",
+      startDate: getToday(),
+      endDate: getFutureDate(3, "month"),
+      description: "",
+    });
   });
 });
